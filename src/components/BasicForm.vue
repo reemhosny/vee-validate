95f72ef798b3f578c12cd28377e0ee2fec4db4c2
reemhosny <reem.hosny@waffarha.com>
2024-01-15 15:59:10 +0200

    feat: :sparkles: basic vee-validate form

---
+++ src/components/BasicForm.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import * as yup from 'yup';
+
+const schema = yup.object({
+  email: yup.string().email().required(),
+  name: yup.string().required(),
+});
+
+function onSubmit(values) {
+  alert(JSON.stringify(values, null, 2));
+}
+</script>
+
+<template>
+  <Form :validation-schema="schema" @submit="onSubmit" class="form">
+    <Field name="email" type="email" />
+    <ErrorMessage name="email" />
+
+    <Field name="name" />
+    <ErrorMessage name="name" />
+
+    <button>Submit</button>
+  </Form>
+</template>
+
+<style>
+.form {
+  gap: 14px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
