17e1dbc76b3ebad7ca36bb465a084ba18513bb87
reemhosny <reem.hosny@waffarha.com>
2024-01-23 19:57:22 +0200

    feat: :sparkles: create custom baseInput  component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,24 @@
+<!-- CustomInput.vue -->
+<template>
+  <input
+    type="text"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    placeholder="Type something..."
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: String,
+  },
+};
+</script>
+
+<style scoped>
+input {
+  padding: 10px;
+  font-size: 16px;
+}
+</style>
